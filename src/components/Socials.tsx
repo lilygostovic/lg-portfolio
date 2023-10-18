b84c0efdd2ccc4a2fed3b695785a78061e41a972
lilygostovic <lily.gostovic@gmail.com>
2023-10-18 10:13:47 -0400

    Add strava and photography website links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,13 +1,14 @@
 import {
+  cameraIcon,
   githubIcon,
   linkedinIcon,
   spotifyIcon,
-  vscoIcon,
+  stravaIcon,
 } from '../images';
 import { StyledDiv } from './common';
 
 export const Socials = () => (
-  <StyledDiv width="180px" display="flex" justifyContent="space-between">
+  <StyledDiv width="225px" display="flex" justifyContent="space-between">
     <a href="https://github.com/lilygostovic" rel="noreferrer" target="_blank">
       <img src={githubIcon} alt="link to github profile" height="30px" />
     </a>
@@ -19,6 +20,13 @@ export const Socials = () => (
       <img src={linkedinIcon} alt="link to linkedin profile" height="30px" />
     </a>
     <a
+      href="https://www.strava.com/athletes/24507964"
+      rel="noreferrer"
+      target="_blank"
+    >
+      <img src={stravaIcon} alt="link to Strava profile" height="30px" />
+    </a>
+    <a
       href="https://open.spotify.com/user/lilygostovic?si=e5b093ac8ad54d74"
       rel="noreferrer"
       target="_blank"
@@ -26,11 +34,11 @@ export const Socials = () => (
       <img src={spotifyIcon} alt="link to spotify profile" height="30px" />
     </a>
     <a
-      href="https://vsco.co/lilygostovic/journal/p/1"
+      href="https://photography.lilygostovic.com"
       rel="noreferrer"
       target="_blank"
     >
-      <img src={vscoIcon} alt="link to vsco profile" height="30px" />
+      <img src={cameraIcon} alt="link to photography website" height="30px" />
     </a>
   </StyledDiv>
 );
