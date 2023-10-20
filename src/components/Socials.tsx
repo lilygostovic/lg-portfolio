80112a75b1125fdf3b92844a9e1c79164cd2079d
lilygostovic <lily.gostovic@gmail.com>
2023-10-20 17:56:53 -0400

    Make socials white color

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -10,35 +10,60 @@ import { StyledDiv } from './common';
 export const Socials = () => (
   <StyledDiv width="225px" display="flex" justifyContent="space-between">
     <a href="https://github.com/lilygostovic" rel="noreferrer" target="_blank">
-      <img src={githubIcon} alt="link to github profile" height="30px" />
+      <img
+        src={githubIcon}
+        alt="link to github profile"
+        height="25px"
+        style={{ filter: "invert(100%) opacity(60%)" }}
+      />
     </a>
     <a
       href="https://www.linkedin.com/in/lily-gostovic/"
       rel="noreferrer"
       target="_blank"
     >
-      <img src={linkedinIcon} alt="link to linkedin profile" height="30px" />
+      <img
+        src={linkedinIcon}
+        alt="link to linkedin profile"
+        height="25px"
+        style={{ filter: "invert(100%) opacity(60%)" }}
+      />
     </a>
     <a
-      href="https://www.strava.com/athletes/24507964"
+      href="https://www.strava.com/athletes/24607964"
       rel="noreferrer"
       target="_blank"
     >
-      <img src={stravaIcon} alt="link to Strava profile" height="30px" />
+      <img
+        src={stravaIcon}
+        alt="link to Strava profile"
+        height="25px"
+        style={{ filter: "invert(100%) opacity(60%)" }}
+      />
     </a>
     <a
       href="https://open.spotify.com/user/lilygostovic?si=e5b093ac8ad54d74"
       rel="noreferrer"
       target="_blank"
     >
-      <img src={spotifyIcon} alt="link to spotify profile" height="30px" />
+      <img
+        src={spotifyIcon}
+        alt="link to spotify profile"
+        height="25px"
+        style={{ filter: "invert(100%) opacity(60%)" }}
+      />
     </a>
     <a
       href="https://photography.lilygostovic.com"
       rel="noreferrer"
       target="_blank"
     >
-      <img src={cameraIcon} alt="link to photography website" height="30px" />
+      <img
+        src={cameraIcon}
+        alt="link to photography website"
+        height="25px"
+        style={{ filter: "invert(100%) opacity(60%)" }}
+      />
     </a>
   </StyledDiv>
 );
